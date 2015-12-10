b4acf4cd086437e44d3c0d03359b0a95a21029a6
GaborK <gabor@lustlab.net>
2015-12-10 15:01:14 +0100

    ...

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ window.onload = function(){
 
 var on = true;
 function flash(){
-    console.log("hello");
     document.body.style.backgroundColor = on ? "white" : "black";
     document.querySelector('#hello').style.color = on ? "black" : "white";
     on = !on;
