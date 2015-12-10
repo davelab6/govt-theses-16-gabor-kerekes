3fa24cdceb1f01413a9ba4c7bc070e0f53ba977d
GaborK <gabor@lustlab.net>
2015-12-10 14:43:33 +0100

    created folders for js and css

--- js/main.js
+++ js/main.js
@@ -11,5 +11,6 @@ var on = true;
 function flash(){
     console.log("hello");
     document.body.style.backgroundColor = on ? "white" : "black";
+    document.querySelector('#title').style.color = on ? "black" : "white";
     on = !on;
 }
